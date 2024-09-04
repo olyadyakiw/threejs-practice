0e2694483d1dc66d34e39c8a9ae7f9550b946ed2
Olha Diakiv <olya.dyakiv26@gmail.com>
2024-09-04 17:52:23 +0200

    add threejs-practice/13-island-scene (continue)

---
+++ 13-island-scene/src/pages/Home.jsx
@@ -0,0 +1,23 @@
+import { Suspense } from "react"
+import { Canvas } from '@react-three/fiber'
+import Loader from "../components/Loader"
+
+const Home = () => {
+  return (
+    <section className="w-full h-screen relative">
+        {/* <div className="absolute top-28 left-0 right-0 z-10 flex items-start justify-center">
+            Popup
+        </div> */}
+        <Canvas 
+            className="w-full h-screen bg-transparent"
+            camera={ { near: 0.1, far: 100,  } }
+        >
+            <Suspense fallback={ <Loader /> }>
+
+            </Suspense>
+        </Canvas>
+    </section>
+  )
+}
+
+export default Home
---
+++ 13-island-scene/src/pages/index.js
@@ -0,0 +1,7 @@
+import About from "./About"
+import Contact from "./Contact"
+import Home from "./Home"
+import Projects from "./Projects"
+
+
+export { Home, About, Projects, Contact }
